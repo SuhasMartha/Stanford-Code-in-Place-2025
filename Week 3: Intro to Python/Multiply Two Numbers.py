bce3bfe4bb3d5be01f61aeb57e6b7ed17f7a3789
Suhas <66237258+SuhasMartha@users.noreply.github.com>
2025-05-24 09:18:45 +0530

    Create Multiply Two Numbers.py

---
+++ Week 3: Intro to Python/Multiply Two Numbers.py
@@ -0,0 +1,11 @@
+def main():
+    print("This program multiplies two numbers.")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = num1 * num2
+    print(result)
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
